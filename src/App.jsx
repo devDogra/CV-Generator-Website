f0111fba2276deebf6d04d1dbc5d4635e4d5c494
DevAryanDogra <devdogra1@gmail.com>
2023-02-07 21:47:01 +0530

    Make personal info form controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,79 @@
 import React from "react"
 import "./App.css"
 
+const exp1 = {
+  type: 'work',
+  organisation: 'google',
+  role: 'sde',
+  startDate: '2019',
+  endDate: 'present'
+}
+
+const exp2 = {
+  type: 'work',
+  organisation: 'twitter',
+  role: 'hr',
+  startDate: '2016',
+  endDate: '2018'
+}
+
+const exp3 = {
+  type: 'study',
+  organisation: 'DTU',
+  role: 'computer engineering',
+  startDate: '2011',
+  endDate: '2015'
+}
+
+
 export default class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      fullname: 'ddedeede',
+      email: 'dedede',
+      phone: '',
+      
+      // experiences: [{
+      //   type: '',
+      //   organisation: '',
+      //   role: '',
+      //   startDate: '',
+      //   endDate: '',
+      // }],
+      experiences: [
+        exp1, exp2, exp3
+      ]
+
+ 
+    }
+    this.handleChange = this.handleChange.bind(this);    
+  }
+  // e is the event of the change, 
+  // inputName is the 'name' attribute of the input
+  // that changed
+  handleChange(e, inputName){
+    this.setState(state => {
+      console.log(state);
+      const newState = {
+        [inputName]: e.target.value + "BOOGIE"
+      }
+      console.log("STATE IS NOW");
+      console.log({...state, ...newState});
+      return newState; 
+    })
+  }
   render(){
     return (
       <>
-        <Form />
+        <ResumeForm 
+        fullname={this.state.fullname}
+        email={this.state.email}
+        phone={this.state.phone}
+        experiences={this.state.experiences}
+        onChange={this.handleChange}
+        />
         <Resume />
       </>
     );
@@ -13,22 +81,39 @@ export default class App extends React.Component{
 }
 
 class PersonalInfoInput extends React.Component{
+  constructor(props){
+    super(props);
+  }
   render(){
+    console.log(this.props.fullname);
     return (
       <>
-        <label htmlFor="">
+        <label  htmlFor="">
           Name
-          <input type="text" />
+          <input 
+          name="fullname"
+          value={this.props.fullname}
+          onChange={e => this.props.onChange(e, "fullname")}
+          type="text" 
+          />
         </label>
 
         <label htmlFor="">
           Email
-          <input type="text" />
+          <input
+          name="email" 
+          value={this.props.email}
+          onChange={e => this.props.onChange(e, "email")}
+          type="text" />
         </label>
 
-        <label htmlFor="">
+        <label  htmlFor="">
           Phone
-          <input type="text" />
+          <input
+          value={this.props.phone}
+          onChange={e => this.props.onChange(e, "phone")}
+          name="phone"
+          type="text" />
         </label>
       </>
 
@@ -39,7 +124,7 @@ class PersonalInfoInput extends React.Component{
 class PeriodInput extends React.Component{
   render(){
     return(
-      <fieldset className="work-period-input">
+      <fieldset className="period-input">
       <legend>Period</legend>
       
       <label htmlFor="">
@@ -58,41 +143,50 @@ class PeriodInput extends React.Component{
 }
 
 class ExperienceInfoInput extends React.Component{
+
  render(){
+  const study = (this.props.type === "study"); 
   return(
     <div className="work-input">
-    <label htmlFor="">
-      Company
-      <input type="text" />
-    </label>
+      <label htmlFor="">
+        {study ? "Institution" : "Company"}
+        <input type="text" />
+      </label>
 
-    <label htmlFor="">
-      Role
-      <input type="text" />
-    </label>
+      <label htmlFor="">
+        {study ? "Degree" : "Role"}
+        <input type="text" />
+      </label>
 
-    <PeriodInput />
-   
-
-    <label htmlFor="">
-      Description
-      <input type="text" />
-    </label>
+      <PeriodInput />
+    
+      {!study && (
+        <label htmlFor="">
+          Description
+          <input type="text" />
+        </label>
+      )}
 
   </div>
   );
  }
 }
 
-class Form extends React.Component{
+class ResumeForm extends React.Component{
 
   render(){
+    console.log(this.props);
     return (
       <form action="">
 
         <h2>Personal Info</h2>
         <div className="personal-info-form">
-          <PersonalInfoInput />
+          <PersonalInfoInput 
+          fullname={this.props.fullname}
+          email={this.props.email}
+          phone={this.props.phone}
+          onChange={this.props.onChange}
+          />
         </div>
 
         <h2>Work experience</h2>
@@ -103,7 +197,7 @@ class Form extends React.Component{
 
         <h2>Education</h2>
         <div className="education-form">
-         <ExperienceInfoInput/>
+         <ExperienceInfoInput type="study" />
           <button type="button">Add education input</button>
         </div>
 
